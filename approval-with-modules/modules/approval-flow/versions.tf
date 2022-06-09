04b56a7d2b4c69ddaa3b9afd8a56286d933b7d61
Ari <Arianna2028@users.noreply.github.com>
2022-06-09 15:48:32 -0400

    feedback: choose quickstart with flat structure

--- approval-with-modules/modules/approval-flow/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    sym = {
-      source  = "terraform.symops.com/symopsio/sym"
-      version = ">= 1.11.2, < 2.0.0"
-    }
-  }
-}
--- approval-with-modules/modules/sym-shared/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "environment_id" {
-  description = "ID for the created environment."
-  value       = sym_environment.this.id
-}
