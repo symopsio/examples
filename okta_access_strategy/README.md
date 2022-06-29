2232a9fdbfa23080501d0ed4e38e0f6c32178270
Leslie Lam <leslie@symops.io>
2022-06-29 11:45:57 -0400

    Implement Okta strategy example

--- okta_access_strategy/README.md
+++ okta_access_strategy/README.md
@@ -1,6 +1,8 @@
 # Okta Access Strategy
 This example illustrates how to implement a Sym Flow that uses an Okta Access Strategy to grant users temporary access to an Okta Group.
 
+https://user-images.githubusercontent.com/10479740/176488922-ac9131ab-015b-4b7b-9c9a-c31bc031c676.mov
+
 ## About Sym
 
 This workflow is just one example of how [Sym Implementers](https://docs.symops.com/docs/sym-for-implementers) use the [Sym SDK](https://docs.symops.com/docs) to create [Sym Flows](https://docs.symops.com/docs/flows).
--- okta_access_strategy/main.tf
+++ okta_access_strategy/main.tf
@@ -2,29 +2,145 @@ provider "sym" {
   org = "sym-example"
 }
 
+############ General AWS Secrets Manager Setup ##############
+
+# Creates an AWS IAM Role that the Sym Runtime can use for execution
+# Allow the runtime to assume roles in the /sym/ path in your AWS Account
+module "runtime_connector" {
+  source  = "terraform.symops.com/symopsio/runtime-connector/sym"
+  version = ">= 1.1.0"
+
+  # The aws/secretsmgr addon is required to access secrets
+  addons = ["aws/secretsmgr"]
+
+  environment = "main"
+}
+
+# An Integration that tells the Sym Runtime resource which AWS Role to assume
+# (The AWS Role created by the runtime_connector module)
+resource "sym_integration" "runtime_context" {
+  type = "permission_context"
+  name = "main-runtime"
+
+  external_id = module.runtime_connector.settings.account_id
+  settings    = module.runtime_connector.settings
+}
+
+# This resource tells Sym which role to use to access your AWS Secrets Manager
+resource "sym_secrets" "this" {
+  type = "aws_secrets_manager"
+  name = "main-sym-secrets"
+
+  settings = {
+    context_id = sym_integration.runtime_context.id
+  }
+}
+
+############ Okta Integration and Secret Setup ##############
+
+# An AWS Secrets Manager Secret to hold your Okta API Key. Set the value with:
+# aws secretsmanager put-secret-value --secret-id "${ENVIRONMENT_NAME}/okta-api-key" --secret-string "YOUR-OKTA-API-KEY"
+resource "aws_secretsmanager_secret" "okta_api_key" {
+  name        = "main/okta-api-key"
+  description = "API Key for Sym to call Okta APIs"
+
+  tags = {
+    SymEnv = "main"
+  }
+}
+
+# This resources tells Sym how to access your Okta API Key.
+# (source = AWS Secrets Manager, access with IAM Role.
+#  path = name of the key in AWS Secrets Manager)
+resource "sym_secret" "okta_api_key" {
+  source_id = sym_secrets.this.id
+  path      = aws_secretsmanager_secret.okta_api_key.name
+}
+
+# The Okta Integration that your Sym Strategy uses to manage your Okta targets
+resource "sym_integration" "okta" {
+  type        = "okta"
+  name        = "main-okta-integration"
+  external_id = "dev-12345.okta.com"
+
+  settings = {
+    # `type=okta` sym_integrations have a required setting `api_token_secret`,
+    # which must point to a sym_secret referencing your Okta API Key
+    api_token_secret = sym_secret.okta_api_key.id
+  }
+}
+
+############ Okta Strategy Setup ##############
+
+# A target Okta group that your Sym Strategy can manage access to
+resource "sym_target" "okta_admin_access" {
+  type  = "okta_group"
+  name  = "main-admin-access"
+  label = "Admin Access"
+
+  settings = {
+    # `type=okta_group` sym_targets have a required setting `group_id`,
+    # which must be the Group ID the requester will be escalated to when this target is selected.
+
+    # The GroupID is visible while in the Okta Admin console, with the Group selected, in the URL of the browser.
+    # Directory > Groups > Select the Group > the ID at the end of the browser's URL.
+    group_id = "00g12345xxx"
+  }
+}
+
+# A target Okta group that your Sym Strategy can manage access to
+resource "sym_target" "okta_s3_access" {
+  type  = "okta_group"
+  name  = "main-s3-access"
+  label = "S3 Write Access"
+
+  settings = {
+    # `type=okta_group` sym_targets have a required setting `group_id`,
+    # which must be the Group ID the requester will be escalated to when this target is selected.
+
+    # The GroupID is visible while in the Okta Admin console, with the Group selected, in the URL of the browser.
+    # Directory > Groups > Select the Group > the ID at the end of the browser's URL.
+    group_id = "00g67890xxx"
+  }
+}
+
+# The Strategy your Flow uses to escalate to Okta Groups
+resource "sym_strategy" "okta" {
+  type           = "okta"
+  name           = "main-okta-strategy"
+  integration_id = sym_integration.okta.id
+
+  # This must be a list of `okta_group` sym_target that users can request to be escalated to
+  targets = [sym_target.okta_admin_access.id, sym_target.okta_s3_access.id]
+}
+
 resource "sym_flow" "this" {
-  name  = "approval"
-  label = "Approval"
+  name  = "okta"
+  label = "Okta Group Request"
 
   template       = "sym:template:approval:1.0.0"
   implementation = "${path.module}/impl.py"
   environment_id = sym_environment.this.id
 
   params = {
+    # By specifying a strategy, this Flow will now be able to manage access (escalate/de-escalate)
+    # to the targets specified in the `sym_strategy` resource.
+    strategy_id = sym_strategy.okta.id
+
     # prompt_fields_json defines custom form fields for the Slack modal that
     # requesters fill out to make their requests.
     prompt_fields_json = jsonencode([
-      {
-        name     = "resource"
-        label    = "What do you need access to?"
-        type     = "string"
-        required = true
-      },
       {
         name     = "reason"
         label    = "Why do you need access?"
         type     = "string"
         required = true
+      },
+      {
+        name           = "duration"
+        type           = "duration"
+        allowed_values = ["30m", "1h"]
+        required       = true
       }
     ])
   }
@@ -39,6 +155,13 @@ resource "sym_environment" "this" {
 
   integrations = {
     slack_id = sym_integration.slack.id
+
+    # The Okta API Key is implicitly available to your Okta Flow's impl.py,
+    # so this line is optional if you only need to use `sym.sdk.integrations.okta` methods in your Okta Flow.
+    #
+    # But if you wish to use the `sym.sdk.integrations.okta` methods in a different Flow in this Environment,
+    # you must include this `okta_id = sym_integration.okta.id` here.
+    okta_id = sym_integration.okta.id
   }
 }
 
@@ -58,4 +181,7 @@ resource "sym_error_logger" "slack" {
 
 resource "sym_runtime" "this" {
   name = "main"
+
+  # Give the Sym Runtime the permissions defined by the runtime_connector module.
+  context_id = sym_integration.runtime_context.id
 }
