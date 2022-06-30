81cdfe728218ce47fa307cf8d524f2c466555db2
Leslie Lam <leslie@symops.io>
2022-06-30 15:47:28 -0400

    Implement pagerduty.is_on_call example
    Co-authored-by: Ari <Arianna2028@users.noreply.github.com>

--- pagerduty_on_call/impl.py
+++ pagerduty_on_call/impl.py
@@ -1,5 +1,7 @@
-from sym.sdk.annotations import reducer
-from sym.sdk.integrations import slack
+from sym.sdk.annotations import hook, reducer
+from sym.sdk.exceptions import SymException
+from sym.sdk.integrations import pagerduty, slack
+from sym.sdk.templates import ApprovalTemplate
 
 
 # Reducers fill in the blanks that your workflow needs in order to run.
@@ -9,3 +11,19 @@ def get_approvers(event):
 
     # allow_self lets the requester approve themself, which is great for testing!
     return slack.channel("#sym-requests", allow_self=True)
+
+
+@hook
+def on_request(event):
+    """If the requester is on-call, auto-approve their requests"""
+    try:
+        if pagerduty.is_on_call(event.user):
+            original_reason = event.payload.fields["reason"]
+            return ApprovalTemplate.approve(reason=f"Auto-approved On-call engineer: {original_reason}️")
+    except SymException as e:
+        # Catch any exceptions, such as an expired PagerDuty Token.
+        # Skip auto-approval and report the error to the error channel.
+        slack.send_message(
+            slack.channel("#sym-requests"),
+            f"A PagerDutyError occurred while checking who's on-call. Falling back to off-call behavior.\n{e}",
+        )
