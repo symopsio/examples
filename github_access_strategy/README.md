4bf0742080fb13ea5aaa96d28b93a11477e8ef9b
Leslie Lam <leslie@symops.io>
2022-07-07 09:59:36 -0400

    Approval Flow Base

---
+++ github_access_strategy/README.md
@@ -0,0 +1,7 @@
+# GitHub Access Strategy
+
+This example illustrates how to implement a Sym Flow that uses an Okta Access Strategy to grant users temporary access to a GitHub Repository.
+
+## About Sym
+
+This workflow is just one example of how [Sym Implementers](https://docs.symops.com/docs/sym-for-implementers) use the [Sym SDK](https://docs.symops.com/docs) to create [Sym Flows](https://docs.symops.com/docs/flows).
---
+++ github_access_strategy/main.tf
@@ -0,0 +1,64 @@
+provider "sym" {
+  org = "sym-example"
+}
+
+resource "sym_flow" "this" {
+  name  = "approval"
+  label = "Approval"
+
+  template       = "sym:template:approval:1.0.0"
+  implementation = "${path.module}/impl.py"
+  environment_id = sym_environment.this.id
+
+  params = {
+    # prompt_fields_json defines custom form fields for the Slack modal that
+    # requesters fill out to make their requests.
+    prompt_fields_json = jsonencode([
+      {
+        name     = "resource"
+        label    = "What do you need access to?"
+        type     = "string"
+        required = true
+      },
+      {
+        name     = "reason"
+        label    = "Why do you need access?"
+        type     = "string"
+        required = true
+      }
+    ])
+  }
+}
+
+############ Basic Environment Setup ##############
+
+# The sym_environment is a container for sym_flows that share configuration values
+# (e.g. shared integrations or error logging)
+resource "sym_environment" "this" {
+  name            = "main"
+  runtime_id      = sym_runtime.this.id
+  error_logger_id = sym_error_logger.slack.id
+
+  integrations = {
+    slack_id = sym_integration.slack.id
+  }
+}
+
+resource "sym_integration" "slack" {
+  type = "slack"
+  name = "main-slack"
+
+  # The external_id for slack integrations is the Slack Workspace ID
+  external_id = "T123ABC"
+}
+
+# This sym_error_logger will output any warnings and errors that occur during
+# execution of a sym_flow to a specified channel in Slack.
+resource "sym_error_logger" "slack" {
+  integration_id = sym_integration.slack.id
+  destination    = "#sym-errors"
+}
+
+resource "sym_runtime" "this" {
+  name = "main"
+}
