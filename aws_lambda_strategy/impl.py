520254545f2bdf1ee5004b1d1865ea37fd10b5d2
Leslie Lam <leslie@symops.io>
2022-07-07 09:59:36 -0400

    Approval Flow Base

--- aws_lambda_strategy/impl.py
+++ aws_lambda_strategy/impl.py
@@ -1,6 +1,5 @@
-from sym.sdk.annotations import hook, reducer
+from sym.sdk.annotations import reducer
 from sym.sdk.integrations import slack
-from sym.sdk.templates import get_step_output
 
 
 # Reducers fill in the blanks that your workflow needs in order to run.
@@ -10,22 +9,3 @@ def get_approvers(event):
 
     # allow_self lets the requester approve themself, which is great for testing!
     return slack.channel("#sym-requests", allow_self=True)
-
-
-@hook
-def after_escalate(event):
-    # `get_step_output` defaults to current event if none is passed.
-    # This is equivalent to `get_step_output("escalate")`
-    output = get_step_output()
-
-    # The output message is defined in `lambda_src/handler.py`
-    slack.send_message(event.get_actor("request"), output["body"]["message"])
-
-
-@hook
-def after_deescalate(event):
-    # This is equivalent to `get_step_output("deescalate")`
-    output = get_step_output()
-
-    # The output message is defined in `lambda_src/handler.py`
-    slack.send_message(event.get_actor("request"), output["body"]["message"])
