fdae930ad73caec81efe9993e1720aba94d87888
Leslie Lam <leslie@symops.io>
2022-07-11 16:27:03 -0400

    Circle CI Example

--- approve_circleci_job/main.tf
+++ approve_circleci_job/main.tf
@@ -2,27 +2,115 @@ provider "sym" {
   org = "sym-example"
 }
 
+############ General AWS Secrets Manager Setup ##############
+
+# Creates an AWS IAM Role that the Sym Runtime can use for execution
+# Allow the runtime to assume roles in the /sym/ path in your AWS Account
+module "runtime_connector" {
+  source  = "terraform.symops.com/symopsio/runtime-connector/sym"
+  version = ">= 1.1.0"
+
+  # The aws/secretsmgr addon is required to access secrets
+  addons = ["aws/secretsmgr"]
+
+  environment = "main"
+}
+
+# An Integration that tells the Sym Runtime resource which AWS Role to assume
+# (The AWS Role created by the runtime_connector module)
+resource "sym_integration" "runtime_context" {
+  type = "permission_context"
+  name = "runtime-main"
+
+  external_id = module.runtime_connector.settings.account_id
+  settings    = module.runtime_connector.settings
+
+}
+
+
+# This resource tells Sym which role to use to access your AWS Secrets Manager
+resource "sym_secrets" "this" {
+  type = "aws_secrets_manager"
+  name = "main"
+
+  settings = {
+    context_id = sym_integration.runtime_context.id
+  }
+}
+
+############ CircleCI Integration and Secret Setup ##############
+
+# An AWS Secrets Manager Secret to hold your Okta API Key.
+# This will be used by Sym `on_approve` hook to resume the CircleCI workflow.
+# Set the value with:
+# aws secretsmanager put-secret-value --secret-id "sym/main/circleci-api-key" --secret-string "YOUR-CIRCLECI-API-KEY"
+
+resource "aws_secretsmanager_secret" "circleci_api_key" {
+  name        = "sym/main/circleci-api-key"
+  description = "CircleCI API key for the Sym deploy flow"
+
+  tags = {
+    # This SymEnv tag is required and MUST match the `environment` in your `runtime_connector` module
+    # because the aws/secretsmgr only grants access to secrets tagged with a matching SymEnv value
+    "SymEnv" = "main"
+  }
+}
+
+# This resource tells Sym how to access your CIRCLECI API Key.
+resource "sym_secret" "circleci_api_key" {
+  # The source of your secrets and the permissions needed to access
+  # i.e. AWS Secrets Manager, access with IAM Role.
+  path      = aws_secretsmanager_secret.circleci_api_key.name
+
+  # Name of the key in AWS Secrets Manager
+  source_id = sym_secrets.this.id
+}
+
+# A Custom Integration that can be included in your `sym_environment` so your CircleCI Secret is available in hooks
+resource "sym_integration" "circleci" {
+  type        = "custom"
+  name        = "circleci"
+
+  # A unique ID for this integration
+  external_id = "symopsio"
+
+  settings = {
+    secret_ids_json = jsonencode([sym_secret.circleci_api_key.id])
+  }
+}
+
+############ API-triggered Approval Flow for CircleCI ##############
+
 resource "sym_flow" "this" {
-  name  = "approval"
-  label = "Approval"
+  name  = "ci-approval"
+  label = "CI Approval"
 
   template       = "sym:template:approval:1.0.0"
   implementation = "${path.module}/impl.py"
   environment_id = sym_environment.this.id
 
   params = {
-    # prompt_fields_json defines custom form fields for the Slack modal that
-    # requesters fill out to make their requests.
+    # allowed_sources_json defines the sources from which this flow can be
+    # invoked from. Valid values: "api", "slack".
+    allowed_sources_json = jsonencode(["api"])
+
+    # prompt_fields_json defines the `flow_inputs` to be passed in the API request body
     prompt_fields_json = jsonencode([
       {
-        name     = "resource"
-        label    = "What do you need access to?"
+        name     = "workflow_url"
+        label    = "CI Workflow URL"
         type     = "string"
         required = true
       },
       {
-        name     = "reason"
-        label    = "Why do you need access?"
+        name     = "merging_user"
+        label    = "User who merged PR"
+        type     = "string"
+        required = true
+      },
+      {
+        name     = "workflow_id"
+        label    = "CircleCI workflow"
         type     = "string"
         required = true
       }
@@ -40,7 +128,8 @@ resource "sym_environment" "this" {
   error_logger_id = sym_error_logger.slack.id
 
   integrations = {
-    slack_id = sym_integration.slack.id
+    slack_id    = sym_integration.slack.id
+    circleci_id = sym_integration.circleci.id
   }
 }
 
--- approve_circleci_job/versions.tf
+++ approve_circleci_job/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     sym = {
       source  = "terraform.symops.com/symopsio/sym"
-      version = ">= 1.11.2, < 2.0.0"
+      version = ">= 1.12.0, < 2.0.0"
     }
   }
 }
