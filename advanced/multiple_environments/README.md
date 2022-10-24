32620d0c61fc1ec4a6bbaa87a8a3d002f3c6e146
Ari <Arianna2028@users.noreply.github.com>
2022-10-24 16:55:25 -0400

    Create a new advanced approvals example that uses modules and environments.

---
+++ advanced/multiple_environments/README.md
@@ -0,0 +1,13 @@
+# Using modules to support multiple environments
+
+This example shows a simple way to refactor the [approvals](/approvals/) example into a module so it may be reused to support multiple environments.
+
+This setup is useful if you want to have separate deployments for testing and production.
+
+## Tutorial
+
+Check out a step-by-step tutorial TODO: [here](https://docs.symops.com/docs/custom-integrations)
+
+## About Sym
+
+This workflow is just one example of how Sym Implementers use the [Sym SDK](https://docs.symops.com/docs) to create [Sym Flows](https://docs.symops.com/docs/sym-access-flows).
---
+++ advanced/multiple_environments/environments/sandbox/main.tf
@@ -0,0 +1,20 @@
+provider "sym" {
+  org = "sym-example"
+}
+
+# Create all the necessary resources for an approval flow using a resuable module.
+# This one will be a sandbox environment for testing new ideas.
+module "sandbox_approval_flow" {
+    source = "../../modules/sym"
+
+    # Resources will contain "main" in their name to differentiate environments.
+    environment_name = "sandbox"
+    error_channel_name = "#sym-sandbox-errors"
+    slack_workspace_id = "T123ABC"
+
+    # Configure the sym_flow to send sandbox requests to their own Slack channel.
+    flow_variables = {
+      request_channel = "#sym-sandbox-requests"      # Slack Channel where requests should go
+      approvers       = "foo@symops.io,bar@myco.com" # Optional safelist of users that can approve requests
+    }
+}
