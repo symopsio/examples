0ce816f3d8c0778477562cab40917f7bdce866b4
Leslie Lam <leslie@symops.io>
2023-05-01 16:32:11 -0400

    uncomment version

--- basic/okta_access_strategy/runtime.tf
+++ basic/okta_access_strategy/runtime.tf
@@ -1,9 +1,7 @@
 # The runtime connector module creates both the AWS and Sym resources required to
 module "runtime_connector" {
   source  = "symopsio/runtime-connector/aws"
-
-  # TODO: uncomment this
-#  version = "~> 2.0"
+  version = "~> 2.0"
 
   environment_name = local.environment_name
 }
