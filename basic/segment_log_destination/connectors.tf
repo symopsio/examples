93341acb35baf27e3efa0413e9cffac5f8c3c001
Ari <Arianna2028@users.noreply.github.com>
2023-05-09 16:21:29 -0400

    Put back the AWS provider

--- basic/segment_log_destination/connectors.tf
+++ basic/segment_log_destination/connectors.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 # The runtime_connector module creates an IAM Role that the Sym Runtime can assume to execute operations in your AWS account.
 module "runtime_connector" {
   source  = "symopsio/runtime-connector/aws"
