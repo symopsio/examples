34e732a24789aec9fbb0414d11dca0c6975da133
Ari <Arianna2028@users.noreply.github.com>
2023-05-09 16:40:00 -0400

    [SYM-4800] Update Tailscale Strategy example to use runtime-connector module 2.x

---
+++ basic/tailscale_ssh_access/connectors.tf
@@ -0,0 +1,15 @@
+locals {
+  aws_region = "us-east-1"
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+# The runtime_connector module creates an IAM Role that the Sym Runtime can assume to execute operations in your AWS account.
+module "runtime_connector" {
+  source  = "symopsio/runtime-connector/aws"
+  version = "~> 2.0"
+
+  environment = local.environment_name
+}
