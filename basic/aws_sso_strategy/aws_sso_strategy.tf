2e1efd953744b92106ffaaf13ea54694577c2709
Ari <Arianna2028@users.noreply.github.com>
2023-05-22 13:27:02 -0400

    [SYM-4791] Update AWS SSO Strategy example to use runtime-connector module 2.x (#76)
    Co-authored-by: Leslie Lam <leslie@symops.io>

--- basic/aws_sso_strategy/aws_sso_strategy.tf
+++ basic/aws_sso_strategy/aws_sso_strategy.tf
@@ -1,47 +1,31 @@
-# Set up a different provider for the SSO connector.
-# This is because you typically will put your Sym resources in a different
-# AWS account from your AWS SSO instance.
-provider "aws" {
-  alias  = "sso"
-  region = "us-east-1"
-
-  # Change this profile name to a valid AWS profile for the AWS account where
-  # your AWS SSO instance lives.
-  profile = "sso"
-}
-
-# Get the AWS Account ID for the SSO profile.
-data "aws_caller_identity" "sso" {
+############ Existing AWS SSO Resources ##############
+data "aws_ssoadmin_instances" "sso" {
   provider = aws.sso
 }
 
-# The AWS IAM Resources that enable Sym to manage SSO Permission Sets
-module "sso_connector" {
-  source  = "symopsio/sso-connector/aws"
-  version = ">= 1.0.0"
+# Get the AWS SSO Permission Set to manage with Sym
+data "aws_ssoadmin_permission_set" "power_user" {
+  provider = aws.sso
 
-  # Provision the SSO connector in the AWS account where your AWS
-  # SSO instance lives.
-  providers = {
-    aws = aws.sso
-  }
-
-  environment = "main"
-
-  # The aws_iam_role.sym_runtime_connector_role resource is defined in `runtime.tf`
-  runtime_role_arns = [aws_iam_role.sym_runtime_connector_role.arn]
+  instance_arn = one(data.aws_ssoadmin_instances.sso.arns)
+  name         = "PowerUser"
 }
 
-# The Integration your Strategy uses to manage SSO Permission Sets
-resource "sym_integration" "sso_context" {
-  type        = "permission_context"
-  name        = "main-sso"
-  external_id = module.sso_connector.settings["instance_arn"]
-  settings    = module.sso_connector.settings
+# Get the AWS SSO Group to manage with Sym
+data "aws_identitystore_group" "admin_users" {
+  provider = aws.sso
+
+  identity_store_id = one(data.aws_ssoadmin_instances.sso.identity_store_ids)
+
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = "Admin SSO Group"
+    }
+  }
 }
 
 ############ SSO Strategy Setup ##############
-
 # A target AWS SSO Permission Set Assignment that your Sym Strategy can manage access to
 resource "sym_target" "power_user" {
   type = "aws_sso_permission_set"
@@ -52,19 +36,33 @@ resource "sym_target" "power_user" {
   settings = {
     # `type=aws_sso_permission_set` sym_targets need both an AWS Permission Set
     # ARN and an AWS Account ID to make an sso account assignment
-    permission_set_arn = "arn:aws:sso:::permissionSet/ssoins-aaaaaaaaaaaaaaaa/ps-aaaaaaaaaaaaaaaa"
-    account_id         = "333333333333"
+    permission_set_arn = data.aws_ssoadmin_permission_set.power_user.arn
+    account_id         = "991756738365"
+  }
+}
+
+# A target AWS SSO Group that your Sym Strategy can manage access to
+resource "sym_target" "admin_sso_group" {
+  type = "aws_sso_group"
+
+  name  = "admin-sso-group"
+  label = "Admins Group"
+
+  settings = {
+    # `type=aws_sso` sym_targets have a required setting `group_id`,
+    # which must be the AWS SSO Group ID the requester will be escalated to when this target is selected.
+    group_id  = data.aws_identitystore_group.admin_users.group_id
   }
 }
 
 # The Strategy your Flow uses to escalate to AWS SSO Permission Sets
 resource "sym_strategy" "aws_sso" {
   type           = "aws_sso"
-  name           = "main-aws-sso"
+  name           = "${local.environment_name}-aws-sso"
   integration_id = sym_integration.sso_context.id
 
   # This must be a list of `aws_sso_permission_set` sym_targets that users can request to be escalated to
-  targets = [sym_target.power_user.id]
+  targets = [sym_target.power_user.id, sym_target.admin_sso_group.id]
 
   settings = {
     instance_arn = module.sso_connector.settings["instance_arn"]
@@ -75,7 +73,7 @@ resource "sym_flow" "this" {
   name  = "aws_sso"
   label = "AWS SSO Access"
 
-  implementation = "${path.module}/impl.py"
+  implementation = file("${path.module}/impl.py")
 
   # The sym_environment resource is defined in `environment.tf`
   environment_id = sym_environment.this.id
---
+++ basic/aws_sso_strategy/connectors.tf
@@ -0,0 +1,60 @@
+locals {
+  aws_region = "us-east-1"
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+############ Runtime Connector Setup ##############
+# The runtime_connector module creates an IAM Role that the Sym Runtime can assume to execute operations in your AWS account.
+module "runtime_connector" {
+  source  = "symopsio/runtime-connector/aws"
+  version = "~> 2.0"
+
+  environment = local.environment_name
+
+  # Allow the Runtime Connector Role to assume IAM Roles in the SSO Account as well.
+  account_id_safelist = [data.aws_caller_identity.sso.account_id]
+}
+
+############ AWS SSO Connector Setup ##############
+# Set up a different provider for the SSO connector.
+# This is because you typically will put your Sym resources in a different
+# AWS account from your AWS SSO instance.
+provider "aws" {
+  alias  = "sso"
+  region = local.aws_region
+
+  # Change this profile name to a valid AWS profile for the AWS account where
+  # your AWS SSO instance lives.
+  profile = "sso"
+}
+
+# Get the AWS Account ID for the SSO profile.
+data "aws_caller_identity" "sso" {
+  provider = aws.sso
+}
+
+# The AWS IAM Resources that enable Sym to manage SSO Permission Sets
+module "sso_connector" {
+  source  = "symopsio/sso-connector/aws"
+  version = "~> 2.0"
+
+  # Provision the SSO connector in the AWS account where your AWS
+  # SSO instance lives.
+  providers = {
+    aws = aws.sso
+  }
+
+  environment = local.environment_name
+  runtime_role_arns = [module.runtime_connector.sym_runtime_connector_role.arn]
+}
+
+# The Integration your Strategy uses to manage SSO Permission Sets
+resource "sym_integration" "sso_context" {
+  type        = "permission_context"
+  name        = "${local.environment_name}-sso"
+  external_id = module.sso_connector.settings["instance_arn"]
+  settings    = module.sso_connector.settings
+}
