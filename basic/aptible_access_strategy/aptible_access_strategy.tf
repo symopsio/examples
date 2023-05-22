b757cc7ebb59e7be8fac85519afcce46d4b89a9a
Leslie Lam <leslie@symops.io>
2023-05-22 13:27:44 -0400

    [SYM-4787] Update Aptible example to use runtime_connector module 2.x (#61)

--- basic/aptible_access_strategy/aptible_access_strategy.tf
+++ basic/aptible_access_strategy/aptible_access_strategy.tf
@@ -1,12 +1,12 @@
 # An AWS Secrets Manager Secret to hold your Aptible Bot Token. Set the value with:
-# aws secretsmanager put-secret-value --secret-id "main/aptible-bot-credentials" --secret-string '{"username":"YOUR_BOT_USERNAME","password":"YOUR_BOT_TOKEN"}'
+# aws secretsmanager put-secret-value --secret-id "sym/${local.environment_name}/aptible-bot-credentials" --secret-string '{"username":"YOUR_BOT_USERNAME","password":"YOUR_BOT_TOKEN"}'
 resource "aws_secretsmanager_secret" "aptible_bot_credentials" {
-  name        = "main/aptible-bot-credentials"
+  name        = "sym/${local.environment_name}/aptible-bot-credentials"
   description = "API Key for Sym to call Aptible APIs"
 
   tags = {
-    # This SymEnv tag is required and MUST match the SymEnv tag in the 
-    # aws_iam_policy.secrets_manager_access in your `secrets.tf` file
+    # This SymEnv tag is required and MUST match the `environment` variable
+    # passed into the `secrets_manager_access` module in your `secrets.tf` file
     SymEnv = local.environment_name
   }
 }
@@ -39,7 +39,7 @@ resource "sym_secret" "aptible_bot_password" {
 # The Aptible Integration that your Sym Strategy uses to manage your Aptible Roles targets
 resource "sym_integration" "aptible" {
   type = "aptible"
-  name = "main-aptible-integration"
+  name = "${local.environment_name}-aptible-integration"
 
   # Your Aptible Organization ID
   external_id = "94a49e57-d046-4d9d-9dbf-f7711e337368"
@@ -58,7 +58,7 @@ resource "sym_integration" "aptible" {
 resource "sym_target" "admin_prod" {
   type = "aptible_role"
 
-  name  = "main-aptible-admin-role"
+  name  = "${local.environment_name}-aptible-admin-role"
   label = "Admin Role"
   settings = {
     # You can find the role IDs by going to the Aptible dashboard and selecting a role. The ID will then be in the URL.
@@ -70,7 +70,7 @@ resource "sym_target" "admin_prod" {
 resource "sym_target" "admin_ro" {
   type = "aptible_role"
 
-  name  = "main-aptible-read-only-role"
+  name  = "${local.environment_name}-aptible-read-only-role"
   label = "Read Only Role"
 
   settings = {
@@ -81,7 +81,7 @@ resource "sym_target" "admin_ro" {
 # The Strategy your Flow uses to escalate to Aptible Roles
 resource "sym_strategy" "aptible" {
   type = "aptible"
-  name = "main-aptible-strategy"
+  name = "${local.environment_name}-aptible-strategy"
 
   # When this strategy is run, Sym Runtime will use the credentials specified
   # in this integration to call Aptible APIs.
@@ -95,7 +95,7 @@ resource "sym_flow" "this" {
   name  = "aptible"
   label = "Aptible Access"
 
-  implementation = "${path.module}/impl.py"
+  implementation = file("${path.module}/impl.py")
 
   # The sym_environment resource is defined in `environment.tf`
   environment_id = sym_environment.this.id
---
+++ basic/aptible_access_strategy/connectors.tf
@@ -0,0 +1,7 @@
+# The runtime_connector module creates an IAM Role that the Sym Runtime can assume to execute operations in your AWS account.
+module "runtime_connector" {
+  source  = "symopsio/runtime-connector/aws"
+  version = "~> 2.0"
+
+  environment = local.environment_name
+}
