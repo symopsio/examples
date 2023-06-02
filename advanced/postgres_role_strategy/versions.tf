18e9eed37ae225169babbb1022051f9c93590d45
Leslie Lam <leslie@symops.io>
2023-06-02 09:53:00 -0400

    [SYM-4835] Update advanced/postgres_role_strategy example (#85)

--- advanced/postgres_role_strategy/versions.tf
+++ advanced/postgres_role_strategy/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = "~> 4.0"
     }
     sym = {
       source  = "symopsio/sym"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
   }
 }
