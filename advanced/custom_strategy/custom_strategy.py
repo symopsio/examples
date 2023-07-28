01de9681980c6caf8c14bbca30c8980d345e31e7
Ari <Arianna2028@users.noreply.github.com>
2023-07-28 12:11:10 -0400

    fix: target_identifier

--- advanced/custom_strategy/custom_strategy.py
+++ advanced/custom_strategy/custom_strategy.py
@@ -26,25 +26,34 @@ class CustomStrategy(AccessStrategy):
         """This method executes after a user's request has been approved. It should elevate
         their permissions in the third party system.
         """
-
         # Get the user's identity in the third party system (in this case, their email).
         requester = self.get_requester_identity(event)
+
+        # To use the identifier from `sym_target.settings.identifier`, we need to fetch the target object.
+        target_identifier = event.payload.fields["target"].settings["identifier"]
 
         # Using the user's ID and requested target, make an API call to a third party system to escalate their permissions.
         payload = {
             "user_id": requester,
-            "resource_id": target_id,
+            "resource_id": target_identifier,
         }
 
         requests.post("https://some-api.invalid/escalate", headers=self.headers(), json=payload)
 
     def deescalate(self, target_id, event):
+        """This method executes after a user's request has expired, or during a 'Revoke' event.
+        It should deescalate their permissions in the third party system.
+        """
+        # Get the user's identity in the third party system (in this case, their email).
         requester = self.get_requester_identity(event)
+
+        # To use the identifier from `sym_target.settings.identifier`, we need to fetch the target object.
+        target_identifier = event.payload.fields["target"].settings["identifier"]
 
         # Using the user's ID and requested target, make an API call to a third party system to deescalate their permissions.
         payload = {
             "user_id": requester,
-            "resource_id": target_id,
+            "resource_id": target_identifier,
         }
 
         requests.post("https://some-api.invalid/deescalate", headers=self.headers(), json=payload)
