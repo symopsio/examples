4a4eca14c9d2083d6f841466905210fc88e84c43
Cassie <49845456+caachz@users.noreply.github.com>
2023-12-13 11:33:31 -0800

    Updates multi_tenant_sso_strategy

--- advanced/multi_tenant_sso_strategy/sso_flow/impl.py
+++ advanced/multi_tenant_sso_strategy/sso_flow/impl.py
@@ -1,12 +1,24 @@
 from sym.sdk.annotations import reducer, hook
 from sym.sdk.integrations import slack
-
+from sym.sdk.notifications import Notification
+from sym.sdk.request_permission import PermissionLevel, RequestPermission
 
 # Reducers fill in the blanks that your workflow needs in order to run.
 # For more information, please see https://docs.symops.com/docs/reducers
 @reducer
-def get_approvers(event):
-    """Route Sym requests to a specified channel."""
+def get_permissions(event):
+    """
+    approve_deny is set here to allow only the users in the sym-requests slack channel to approve or deny a request
+    allow_self lets the requester approve themself, which is great for testing!
+    """
+    return RequestPermission(
+        webapp_view=PermissionLevel.ADMIN,
+        approve_deny=slack.users_in_channel("#sym-requests"),
+        allow_self_approval=True
+    )
 
-    # Make sure that this channel has been created in your workspace!
-    return slack.channel("#sym-requests", allow_self=True)
+@reducer
+def get_request_notifications(event):
+    """Send Sym requests to the sym-requests slack channel"""
+
+    return [Notification(destinations=[slack.channel("#sym-requests")])]
