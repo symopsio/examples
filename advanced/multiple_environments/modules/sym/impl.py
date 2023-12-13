73902d9bd7deea9b083e97706e11e9fd2d1faee2
Cassie <49845456+caachz@users.noreply.github.com>
2023-12-13 11:37:33 -0800

    Updates multiple_environments, did not apply

--- advanced/multiple_environments/modules/sym/impl.py
+++ advanced/multiple_environments/modules/sym/impl.py
@@ -1,17 +1,33 @@
 from sym.sdk.annotations import hook, reducer
 from sym.sdk.integrations import slack
 from sym.sdk.templates import ApprovalTemplate
+from sym.sdk.notifications import Notification
+from sym.sdk.request_permission import PermissionLevel, RequestPermission
 
 
 # Reducers fill in the blanks that your workflow needs in order to run.
 @reducer
-def get_approvers(event):
-    """Route Sym requests to a channel specified in the sym_flow."""
+def get_permissions(event):
+    """
+    approve_deny is set here to allow only the users in the sym-requests slack channel to approve or deny a request
+    allow_self lets the requester approve themself, which is great for testing!
+    """
 
     flow_vars = event.flow.vars
 
-    # allow_self lets the requester approve themself, which is great for testing!
-    return slack.channel(flow_vars["request_channel"], allow_self=True)
+    return RequestPermission(
+        webapp_view=PermissionLevel.ADMIN,
+        approve_deny=slack.users_in_channel(flow_vars["request_channel"]),
+        allow_self_approval=True
+    )
+
+@reducer
+def get_request_notifications(event):
+    """Send Sym requests to the sym-requests slack channel"""
+
+    flow_vars = event.flow.vars
+
+    return [Notification(destinations=[slack.channel(flow_vars["request_channel"])])]
 
 
 # Hooks let you change the control flow of your workflow.
