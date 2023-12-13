ecb0d91e11cdfad5ee03c09ac4a821bab242a699
Cassie <49845456+caachz@users.noreply.github.com>
2023-12-13 11:30:12 -0800

    Updates k9_s3_target, did not apply

--- advanced/k9_s3_target/impl.py
+++ advanced/k9_s3_target/impl.py
@@ -1,13 +1,24 @@
 from sym.sdk.annotations import hook, reducer
 from sym.sdk.integrations import slack
 from sym.sdk.notifications import Notification
+from sym.sdk.request_permission import PermissionLevel, RequestPermission
 
 
 # Reducers fill in the blanks that your workflow needs in order to run.
 @reducer
+def get_permissions(event):
+    """
+    approve_deny is set here to allow only the users in the sym-requests slack channel to approve or deny a request
+    allow_self lets the requester approve themself, which is great for testing!
+    """
+    return RequestPermission(
+        webapp_view=PermissionLevel.ADMIN,
+        approve_deny=slack.users_in_channel("#sym-requests"),
+        allow_self_approval=True
+    )
+
+@reducer
 def get_request_notifications(event):
-    """Route Sym requests to a channel specified in the sym_flow."""
+    """Send Sym requests to the sym-requests slack channel"""
 
     return [Notification(destinations=[slack.channel("#sym-requests")])]
-
-# TODO: Add a get_permissions reducer that only sets allow_self=True
