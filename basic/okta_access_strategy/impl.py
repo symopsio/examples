73f8245c836a0db87e457756555f3fa6b6e19565
Cassie <49845456+caachz@users.noreply.github.com>
2023-12-12 16:35:43 -0800

    Updates okta_access_strategy

--- basic/okta_access_strategy/impl.py
+++ basic/okta_access_strategy/impl.py
@@ -1,11 +1,23 @@
 from sym.sdk.annotations import hook, reducer
 from sym.sdk.integrations import slack
-
+from sym.sdk.notifications import Notification
+from sym.sdk.request_permission import PermissionLevel, RequestPermission
 
 # Reducers fill in the blanks that your workflow needs in order to run.
 @reducer
-def get_approvers(event):
-    """Route Sym requests to a channel specified in the sym_flow."""
+def get_permissions(event):
+    """
+    approve_deny is set here to allow only the users in the sym-requests slack channel to approve or deny a request
+    allow_self lets the requester approve themself, which is great for testing!
+    """
+    return RequestPermission(
+        webapp_view=PermissionLevel.ADMIN,
+        approve_deny=slack.users_in_channel("#sym-requests"),
+        allow_self_approval=True
+    )
 
-    # allow_self lets the requester approve themself, which is great for testing!
-    return slack.channel("#sym-requests", allow_self=True)
+@reducer
+def get_request_notifications(event):
+    """Send Sym requests to the sym-requests slack channel"""
+
+    return [Notification(destinations=[slack.channel("#sym-requests")])]
