c3db500cf52758eb54f8907402e7c63208a7ab0a
Cassie <49845456+caachz@users.noreply.github.com>
2023-12-12 16:44:24 -0800

    Updates okta_sdk

--- basic/okta_sdk/impl.py
+++ basic/okta_sdk/impl.py
@@ -1,17 +1,39 @@
 from sym.sdk.annotations import hook, reducer
 from sym.sdk.integrations import okta, slack
 from sym.sdk.templates import ApprovalTemplate
+from sym.sdk.notifications import Notification
+from sym.sdk.request_permission import PermissionLevel, RequestPermission
 
 
 # Reducers fill in the blanks that your workflow needs in order to run.
 @reducer
-def get_approvers(event):
-    """Route Sym requests to a channel based on the user's Okta profile."""
+def get_permissions(event):
+    """
+    approve_deny is set here to allow only the users in the sym-requests slack channel to approve or deny a request
+    allow_self lets the requester approve themself, which is great for testing!
+    """
+    department = get_user_department(event.user)
+    approve_deny = slack.users_in_channel("#sym-requests")
+    if department == "engineering":
+        approve_deny = slack.users_in_channel("#eng-requests")
+
+    return RequestPermission(
+        webapp_view=PermissionLevel.ADMIN,
+        approve_deny=approve_deny,
+        allow_self_approval=True
+    )
+
+@reducer
+def get_request_notifications(event):
+    """Send Sym requests to the sym-requests slack channel"""
+    notifications = []
     department = get_user_department(event.user)
     if department == "engineering":
-        return slack.channel("#eng-requests", allow_self=True)
+        notifications.append(Notification(destinations=[slack.channel("#eng-requests")]))
+    else:
+        notifications.append(Notification(destinations=[slack.channel("#sym-requests")]))
 
-    return slack.channel("#sym-requests", allow_self=True)
+    return notifications
 
 
 def get_user_department(user):
